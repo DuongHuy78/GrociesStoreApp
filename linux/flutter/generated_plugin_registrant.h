bcb37ab37b618d64ccafe01be8db798127249b93
Le Duong Huy <leduonghuy.7.8.5@gmail.com>
2025-09-14 14:10:17 +0700

    merge: develop to develop

--- linux/flutter/generated_plugin_registrant.h
+++ linux/flutter/generated_plugin_registrant.h
@@ -7,7 +7,7 @@
 #ifndef GENERATED_PLUGIN_REGISTRANT_
 #define GENERATED_PLUGIN_REGISTRANT_
 
-#include <flutter_linux/flutter_linux.h>
+#include <"D:\Downloads\setupFlutterAndroid\flutter\engine\src\flutter\shell\platform\linux\public\flutter_linux\flutter_linux.h">
 
 // Registers Flutter plugins.
 void fl_register_plugins(FlPluginRegistry* registry);
